25b3c53e3a6c1ec9d8383cea0e1710360627e510
Sarthak Anil Janrao <119600876+Sarthakjanrao07@users.noreply.github.com>
2024-12-04 09:38:31 +0530

    app.py

--- app.py
+++ app.py
@@ -96,8 +96,9 @@ def predict_image(img, model=disease_model):
     :return: prediction (string)
     """
     transform = transforms.Compose([
-        transforms.Resize(256),
+        transforms.Resize((256, 256)),
         transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
     image = Image.open(io.BytesIO(img))
     img_t = transform(image)
