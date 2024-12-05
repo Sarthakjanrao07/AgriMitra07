ec230fc0f2b06d350a61b3b2899958e00f923fdb
Sarthak Anil Janrao <119600876+Sarthakjanrao07@users.noreply.github.com>
2024-12-05 12:22:56 +0530

    app.py

--- app.py
+++ app.py
@@ -235,4 +235,4 @@ def disease_prediction():
 
 # ===============================================================================================
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
